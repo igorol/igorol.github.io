ea1aa4803e49940e778965c7369e951b7a4875af
Igor Oliveira <igor.oliveira@meteogroup.com>
2019-09-22 20:22:56 +0200

    implemented speed slider

--- spiro/sketch.js
+++ spiro/sketch.js
@@ -1,11 +1,9 @@
 
-let fr = 24;
 let t = 0;
-let rho = 40;
-let bigSlider, smallSlider, rhoSlider, ClearButton;
+let bigSlider, smallSlider, rhoSlider, frSLider, ClearButton;
 
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
+  c = createCanvas(window.innerWidth, window.innerHeight);
   background(255);
 
   strokeWeight(1);
@@ -18,7 +16,8 @@ function setup() {
   bigSlider.position(20, 40);
   smallSlider = createSlider(10, 600, 20, 1);
   smallSlider.position(20, 60);
-
+  frSLider = createSlider(24, 100, 24, 1);
+  frSLider.position(20, 80);
 
   text('Rho - Pen point distance from center = ' + rhoSlider.value(),
        rhoSlider.x * 2 + rhoSlider.width,
@@ -29,39 +28,43 @@ function setup() {
        55);
 
   text('r - Inner wheel radius = ' + smallSlider.value(),
-      smallSlider.x * 2 + smallSlider.width,
-      75);
+       smallSlider.x * 2 + smallSlider.width,
+       75);
 
-
+  text('Speed',
+       frSLider.x * 2 + frSLider.width,
+       95);
 
   // create clear button
   ClearButton = createButton('Clear');
   ClearButton.position(35, 105);
   ClearButton.mousePressed(clearLines);
 
+  // create save button
+  saveButton = createButton('Save Image');
+  saveButton.position(95, 105);
+  saveButton.mousePressed(saveCanvas(c, 'myCanvas', 'png'));
 
 }
 
 function draw() {
 
-  frameRate(24);
   translate(displayWidth/2,displayHeight/2);
-
-
   noFill();
   r = smallSlider.value();
   R = bigSlider.value();
   rho = rhoSlider.value();
+  fr = frSLider.value();
+  frameRate(fr);
 
   delta = 0.02;
-
   if (t == 0) {
     ref_rho = rho;
     ref_r = r;
     ref_R = R;
+    ref_fr = fr;
   }
   else {
-
     // logic for drawing lines
     x = (R - r)*cos(t) + rho*cos(((R-r)/r)*t);
     y = (R - r)*sin(t) - rho*sin(((R-r)/r)*t);
@@ -71,28 +74,11 @@ function draw() {
     strokeWeight(2);
     line(past_x, past_y, x, y);
       //
-      if (rhoSlider.value() != ref_rho | smallSlider.value() != ref_r | bigSlider.value() != ref_R){
+      if (rhoSlider.value() != ref_rho | smallSlider.value() != ref_r | bigSlider.value() != ref_R)
+      {
         clearLines();
       }
-
   }
-
-
-
-
-
-  // translate(-displayWidth/2,-displayHeight/2);
-  // text('Rho - Pen point distance from center = ' + rhoSlider.value(),
-  //      rhoSlider.x * 2 + rhoSlider.width - displayWidth/2,
-  //      35 - displayHeight/2);
-  //
-  // text('R - Outside wheel radius = ' + bigSlider.value(),
-  //      bigSlider.x * 2 + bigSlider.width - displayWidth/2,
-  //      55 - displayHeight/2);
-  //
-  // text('r - Inner wheel radius = ' + smallSlider.value(),
-  //     smallSlider.x * 2 + smallSlider.width - displayWidth/2,
-  //     75 - displayHeight/2);
 
   t += delta;
 }
@@ -116,5 +102,8 @@ function clearLines()
   text('r - Inner wheel radius = ' + smallSlider.value(),
       smallSlider.x * 2 + smallSlider.width - displayWidth/2,
       75 - displayHeight/2);
+  text('Speed', frSLider.x * 2 + frSLider.width - displayWidth/2,
+        95 - displayHeight/2);
+
   draw();
 }
